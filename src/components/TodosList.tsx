5bf703729dce6f3abc22b93b34585e8ac7fbf733
Imran Nazir <imran.nazir@yahoo.co.uk>
2024-12-29 12:35:00 +0000

    tidies up logic of todo component

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -1,13 +1,17 @@
 import type { FormEvent } from "react";
 import { useState } from "react";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import { Input } from "./ui/input";
 
 export const TodosList: React.FC = () => {
   const [list, setList] = useState<string[]>(["1", "2"]);
+  const [input, setInput] = useState<string>("");
 
   async function submit(e: FormEvent) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    
+
     const response = await fetch("/api/todos", {
       method: "POST",
       body: formData,
@@ -15,23 +19,35 @@ export const TodosList: React.FC = () => {
 
     const data = await response.json();
 
+    setInput("")
     setList((prev) => [...prev, data.todo.toString()]);
   }
 
   return (
-    <>
-      <ul>
-        {list.map((listItem, idx) => (
-          <li key={idx}>{listItem}</li>
-        ))}
-      </ul>
-      <form onSubmit={submit}>
-        <label htmlFor="todo">
-          Todo
-          <input type="text" name="todo" required />
-        </label>
-        <button className="">Add</button>
-      </form>
-    </>
+    <Card>
+      <CardHeader></CardHeader>
+      <CardContent>
+        <ul>
+          {list.map((listItem, idx) => (
+            <li key={idx}>{listItem}</li>
+          ))}
+        </ul>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <form onSubmit={submit}>
+          <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input
+              type="text"
+              name="todo"
+              placeholder="Enter a new task"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              required
+            />
+            <Button type="submit">Add</Button>
+          </div>
+        </form>
+      </CardFooter>
+    </Card>
   );
 };
