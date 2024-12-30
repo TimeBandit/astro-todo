67fdbec688669b729a9a33b7faeecb534556c2a3
Imran Nazir <imran.nazir@yahoo.co.uk>
2024-12-30 20:29:29 +0000

    initial auth code

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,12 @@ import react from '@astrojs/react';
 
 import netlify from '@astrojs/netlify';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), auth()],
   adapter: netlify(),
 });
