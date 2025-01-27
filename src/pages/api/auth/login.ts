d0c086f83de1b34cc0513bffe0a0f82f873d7518
Imran Nazir <imran.nazir@yahoo.co.uk>
2025-01-27 17:32:07 +0000

    feat: redeisng of main page

---
+++ src/pages/api/auth/login.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from "astro";
+
+export const prerender = false;
+
+export const GET: APIRoute = async (context) => {
+  context.cookies.set("access_token", {
+    // Match the original cookie settings
+    path: "/",
+    secure: true,
+    sameSite: "strict",
+  });
+
+  return new Response(
+    JSON.stringify({
+      message: "Logged out successfully",
+    }),
+    { status: 200 }
+  );
+};
