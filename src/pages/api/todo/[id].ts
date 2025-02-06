c6323c5bb77cc4f56e40ba9ee048cefe7983cae7
Imran Nazir <imran.nazir@yahoo.co.uk>
2025-02-06 17:22:38 +0000

    feat: added spartial for new list items

--- src/pages/api/todo/[id].ts
+++ src/pages/api/todo/[id].ts
@@ -2,6 +2,19 @@ import type { APIRoute } from "astro";
 
 export const prerender = false;
 
+export const PUT: APIRoute = async ({ params, request }) => {
+  let data;
+  try {
+    data = await request.formData(); // * Blog
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("updating id: ", params, data);
+
+  return new Response(null, { status: 200 });
+};
+
 export const DELETE: APIRoute = ({ params }) => {
   console.log("deleting id: ", params);
   return new Response(null, { status: 200 });
